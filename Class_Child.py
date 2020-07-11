52edb51078621e1c5bc4a68b81f95042b6481efb
Max <maximignatyev7@gmail.com>
2020-07-10 23:11:20 -0700

    Class_Child file
    Added Class_Child file

---
+++ Class_Child.py
@@ -0,0 +1,17 @@
+
+
+class pizza:
+    shape = "round"
+    dough = "thick"
+
+class exclusivePizza(pizza):
+    taste = "sweet"
+    superTopp = "ferrero"
+
+
+class cheapPizza(pizza):
+    state = "uncook"
+    toppings = "olives"
+    
+    
+    
